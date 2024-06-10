038fba2d85df75a2dcfc6c395c554678d0135474
Sogoid <diogos_ribeiro@outlook.com>
2024-06-10 14:11:53 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dragon-Ball-Z-Ionic',
+  webDir: 'www'
+};
+
+export default config;
